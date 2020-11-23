24458aeed6f6710b9e2d50f71049fc02108d0e06
James Downer <downer.james.256k@gmail.com>
2020-11-22 22:32:37 -0700

    Add Color Selector to app list

--- src/applist.js
+++ src/applist.js
@@ -2,5 +2,6 @@ const APPS = {
   'Polygon Generator': 'lib/Polygon-HTML5JS/index.html',
   'Tree Fractal Generator': 'lib/TreeFractal-HTML5JS/index.html',
   'Unicode Transformer': 'lib/UnicodeTransformer/unicode-transformer.html',
+  'Color Selector': 'lib/ColorSelector/index.html',
 };
 
