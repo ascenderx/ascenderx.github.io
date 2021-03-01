896cc64436e91ddd3b90564778951f956a296752
James Downer <downer.james.256k@gmail.com>
2021-02-28 18:22:56 -0700

    Add Japanese Kanji (Kyouiku)

--- src/applist.js
+++ src/applist.js
@@ -1,4 +1,5 @@
 const APPS = {
+  'Ky\u014diku Kanji': 'lib/Japanese-Kanji-Web/kyouiku.html',
   'Polygon Generator': 'lib/Polygon-HTML5JS/index.html',
   'Tree Fractal Generator': 'lib/TreeFractal-HTML5JS/index.html',
   'Unicode Transformer': 'lib/UnicodeTransformer/unicode-transformer.html',
