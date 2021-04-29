cbbe94b6f8775abbd35c27a70fb7ed730ad39662
James Downer <downer.james.256k@gmail.com>
2021-04-28 22:15:58 -0600

    Fix WebGL Complex Functions href

--- src/applist.js
+++ src/applist.js
@@ -2,7 +2,7 @@ const APPS = {
   'Ky\u014diku Kanji': 'lib/Japanese-Kanji-Web/kyouiku.html',
   'Polygon Generator': 'lib/Polygon-HTML5JS/index.html',
   'Tree Fractal Generator': 'lib/TreeFractal-HTML5JS/index.html',
-  'WebGL Complex Functions': 'lib/WebGLExample/index.html',
+  'WebGL Complex Functions': 'lib/WebGLExamples/index.html',
   'Unicode Transformer': 'lib/UnicodeTransformer/unicode-transformer.html',
   'Color Selector': 'lib/ColorSelector/index.html',
 };
